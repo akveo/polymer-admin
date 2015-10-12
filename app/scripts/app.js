e591e51b34fd18a27dfd74134098a2e1156d32b9
Nasta <an.kartul@gmail.com>
2015-10-12 21:52:39 +0300

    fix(refactoring): project restructure, refactoring

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,6 @@
-/*
-Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-
 (function(document) {
   'use strict';
 
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
 
   app.displayInstalledToast = function() {
@@ -22,21 +10,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     }
   };
 
-  // Close drawer after menu item is selected if drawerPanel is narrow
-  app.onDataRouteClick = function() {
-    var drawerPanel = document.querySelector('#paperDrawerPanel');
-    if (drawerPanel.narrow) {
-      drawerPanel.closeDrawer();
-    }
-
-    var charts = document.getElementsByTagName('google-chart');
-    setTimeout(function(){
-      for (var i = 0; i < charts.length; i++) {
-        charts[i].drawChart();
-      }
-    }, 0);
-  };
-
+  // Toggle Drawer
   app.toggleDrawer = function() {
     var drawerPanel = document.getElementById('paperDrawerPanel');
     if (drawerPanel.narrow) {
@@ -51,18 +25,18 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     document.getElementById('mainContainer').scrollTop = 0;
   };
 
+  // Initial widgets cols number
   app.cols = '2';
 
-  app.toggleSubmenu = function(e) {
-    e.target.parentNode.classList.toggle('submenu-expanded');
-  };
-
+  // Firebase location
   app.location = 'https://polymer-admin.firebaseio.com';
 
+  // Sign out user
   app.signOut = function() {
     this.$.data.signOut();
   };
 
+  // Sign in user
   app.signIn = function() {
     this.$.data.signIn();
   };
